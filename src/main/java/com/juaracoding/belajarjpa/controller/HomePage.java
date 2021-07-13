94c7e5d40a48539a9c8d2e4ca6b31a49ae8a3736
dewabrata <dewabrata@gmail.com>
2021-07-13 15:09:54 +0700

    add login logic simple

--- src/main/java/com/juaracoding/belajarjpa/controller/HomePage.java
+++ src/main/java/com/juaracoding/belajarjpa/controller/HomePage.java
@@ -1,8 +1,11 @@
 package com.juaracoding.belajarjpa.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -28,16 +31,24 @@ public class HomePage {
 	ContactRepository contactRepo;
 	
 	@GetMapping("/")
-	public String viewIndex(Model model, HttpServletRequest request) {
+	public String viewIndex(Model model, HttpSession session) {
 		
-		//ambil session
-		//check session ada?
 		
-		String sesi = (String) request.getSession().getAttribute("username");
 		
+		List<String> lstUsrLogin = (List<String>) session.getAttribute("USER_LOGIN") ;
+		if (lstUsrLogin == null) {
+			
+			model.addAttribute("textLoginStatus" , "Login");
+			return "redirect:/login";
+			
+		}
+		
+		
+		
+		model.addAttribute("textLoginStatus" , "Logout");
 		model.addAttribute("bookingmodel" , new BookingModel());
 		model.addAttribute("listActivities", this.contactRepo.findAll());
-		model.addAttribute("namanya", sesi);
+		
 	
 		return "index.html";
 	}
@@ -96,13 +107,48 @@ public class HomePage {
 		return "redirect:/contact";
 	}
 	
-	
 	@GetMapping("/login")
-	public String viewLogin(Model model, HttpServletRequest request) {
+	public String viewLogin(Model model) {
 		
-	
+		
+		
 		return "login.html";
 	}
+	
+	
+	
+	
+	@PostMapping("/ceklogin")
+	public String cekLogin(@RequestParam("email") String email , @RequestParam("pass")String password , HttpServletRequest request) {
+		
+		List<String> lstUsrLogin = (List<String>) request.getAttribute("USER_LOGIN") ;
+		if (lstUsrLogin == null) {
+			lstUsrLogin = new ArrayList<String>();
+			
+			
+		}
+		
+		
+		if(email.equals("dewabrata@gmail.com") && password.equalsIgnoreCase("12345")) {
+			lstUsrLogin.add(email);
+			request.getSession().setAttribute("USER_LOGIN", lstUsrLogin);
+			
+			return "redirect:/";
+		}else {
+	
+			return "redirect:/login";
+		}
+	}
+	
+	@GetMapping("/logout")
+	public String viewLogOut(HttpServletRequest request, Model model) {
+		
+		request.getSession().invalidate();		
+		
+		
+		return "redirect:/";
+	}
+	
 
 
 }
